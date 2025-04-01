dd9f8ff4152245fe45f98e11c4f2252d4f33a660
XiaobingHuang <66129397+xiaobinghuang@users.noreply.github.com>
2025-04-01 15:55:10 -0400

    Initial commit

---
+++ src/test/java/com/example/basketballtournament/BasketballtournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basketballtournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballtournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
